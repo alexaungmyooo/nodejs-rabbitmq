b063ec45db8304fac9f2f3fbee7a8fcf1bcb86b4
Aung Myo Oo <aungmyooo@Aungs-MacBook-Pro.local>
2023-02-25 17:47:27 +0630

    wait for confirms

--- messages/producer.js
+++ messages/producer.js
@@ -1,5 +1,5 @@
-const rabbitmq = require('../lib/rabbitmq');
-const config = require('../config/config');
+const rabbitmq = require("../lib/rabbitmq");
+const config = require("../config/config");
 
 async function produceMessage(message) {
   try {
@@ -8,17 +8,19 @@ async function produceMessage(message) {
     const queue = config.rabbitmq.queue;
     const key = config.rabbitmq.routingKey;
 
-    await channel.assertExchange(exchange, 'direct', { durable: true });
+    await channel.assertExchange(exchange, "direct", { durable: true });
     await channel.assertQueue(queue, { durable: true });
     await channel.bindQueue(queue, exchange, key);
 
     const messageBuffer = Buffer.from(message);
     await channel.publish(exchange, key, messageBuffer);
     console.log(`Message sent: ${message}`);
+    //await rabbitmq.close();
+    await channel.waitForConfirms();
     await rabbitmq.close();
   } catch (error) {
-    console.error('Error producing message', error);
+    console.error("Error producing message", error);
   }
 }
 
-produceMessage('Hello, world!');
+produceMessage("Hello, world!");
